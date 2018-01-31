956f71e434299b5958df844006527691df30a2a8
Teddy <teddy689@qq.com>
2018-01-31 16:15:51 +0800

    1

--- html5/js/widget/widgetDock.js
+++ html5/js/widget/widgetDock.js
@@ -7910,7 +7910,8 @@ _$6C.prototype._$qJ = function () {
 _$L.prototype = new _$17;
 
 function _$L() {
-};_$L.prototype._$2G = function (_$nz, _$dk, _$p8, _$8b) {
+};
+_$L.prototype._$2G = function (_$nz, _$dk, _$p8, _$8b) {
     this._$ho._$2w(_$nz, _$dk, _$p8, _$8b);
 };
 _$L.prototype._$67 = function (_$9g, _$nA, _$97) {
