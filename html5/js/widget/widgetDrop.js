a484eed50aae966319482f1bdcfa4bb40bffd1f3
Teddy <teddy689@qq.com>
2018-03-21 20:48:29 +0800

    1

--- html5/js/widget/widgetDrop.js
+++ html5/js/widget/widgetDrop.js
@@ -1,118 +1,115 @@
-var widgetDropController = {
-    name: "widgetDropController",
+function WidgetDropController() {
+};
+WidgetDropController.name = "WidgetDropController";
 
-    init: function (d) {
-        logController.log(this.name, "->", arguments.callee.name);
+WidgetDropController.prototype.init = function (d) {
+    logController.log(this.name, "->", arguments.callee.name);
 
-        // 拖拽的目标对象document监听drop并防止浏览器打开客户端的图片
-        document.ondragover = widgetDropController.documentOndragover;
-        document.ondrop = widgetDropController.documentOndrop;
+    // 拖拽的目标对象document监听drop并防止浏览器打开客户端的图片
+    document.ondragover = this.documentOndragover;
+    document.ondrop = this.documentOndrop;
 
-        var dropList = document.getElementsByClassName("widgetDrop");
-        var $dropList = $(dropList);
-        $dropList.each(function (dropIndex) {
-            var $dropItem = $(dropList[dropIndex]);
-            $dropItem.context.ondragover = widgetDropController.itemOndragover;
-            $dropItem.context.ondragenter = widgetDropController.itemOndragenter;
-            $dropItem.context.ondragleave = widgetDropController.itemOndragleaver;
-            $dropItem.context.ondrop = widgetDropController.itemOndrop;
-        })
-    },
+    var dropList = document.getElementsByClassName("widgetDrop");
+    var $dropList = $(dropList);
+    $dropList.each(function (dropIndex) {
+        var $dropItem = $(dropList[dropIndex]);
+        $dropItem.context.ondragover = this.itemOndragover;
+        $dropItem.context.ondragenter = this.itemOndragenter;
+        $dropItem.context.ondragleave = this.itemOndragleaver;
+        $dropItem.context.ondrop = this.itemOndrop;
+    })
+};
+WidgetDropController.prototype.documentOndragover = function (e) {
+    e.preventDefault(); // 只有在ondragover中阻止默认行为才能触发ondrop而不是ondragleave
+};
+WidgetDropController.prototype.documentOndrop = function (e) {
+    e.preventDefault(); // 阻止ondrop的默认行为（在新窗口中打开拖进的图片）
+};
+WidgetDropController.prototype.itemOndragover = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+};
+WidgetDropController.prototype.itemOndragenter = function (e) {
+    e.currentTarget.style.borderColor = 'gray';
+    e.currentTarget.style.backgroundColor = 'white';
+    e.stopPropagation();
+    e.preventDefault();
+};
+WidgetDropController.prototype.itemOndragleaver = function (e) {
+    e.currentTarget.style.borderColor = 'silver';
+    e.currentTarget.style.backgroundColor = 'transparent';
+    e.stopPropagation();
+    e.preventDefault();
+};
+WidgetDropController.prototype.itemOndrop = function (e) {
+    e.currentTarget.style.borderColor = 'silver';
+    e.currentTarget.style.backgroundColor = 'transparent';
+    e.stopPropagation();
+    e.preventDefault();
+    this.handleFiles(e.dataTransfer.files);
+};
+WidgetDropController.prototype.handleFiles = function (files) {
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        this.readFile(file);
+    }
+};
+WidgetDropController.prototype.readFile = function (file) {
+    alert("a");
+    if (file.type.match(/image*/)) {
+        var img = document.createElement("img");
+        img.classList.add("obj");
+        img.file = file;
 
-    documentOndragover : function (e) {
-        e.preventDefault(); // 只有在ondragover中阻止默认行为才能触发ondrop而不是ondragleave
-    },
-    documentOndrop : function (e) {
-        e.preventDefault(); // 阻止ondrop的默认行为（在新窗口中打开拖进的图片）
-    },
-    itemOndragover : function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-    },
-    itemOndragenter : function (e) {
-        e.currentTarget.style.borderColor = 'gray';
-        e.currentTarget.style.backgroundColor = 'white';
-        e.stopPropagation();
-        e.preventDefault();
-    },
-    itemOndragleaver : function (e) {
-        e.currentTarget.style.borderColor = 'silver';
-        e.currentTarget.style.backgroundColor = 'transparent';
-        e.stopPropagation();
-        e.preventDefault();
-    },
-    itemOndrop : function (e) {
-        e.currentTarget.style.borderColor = 'silver';
-        e.currentTarget.style.backgroundColor = 'transparent';
-        e.stopPropagation();
-        e.preventDefault();
-        widgetDropController.handleFiles(e.dataTransfer.files);
-    },
+        var item = document.getElementById("widgetDrop_root");
+        item.appendChild(img);
 
-    handleFiles: function (files) {
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            widgetDropController.readFile(file);
+        var reader = new FileReader();
+        reader.onload = function () {
+            console.log(reader.result);
+            img.src = reader.result;
         }
-    },
+        reader.readAsDataURL(file);
+    } else {
+        // <input id="File1" type="file" value="" name ="file"/ >
+        var input = document.createElement("input");
+        input.classList.add("obj");
 
-    readFile: function (file) {
-        alert("a");
-        if (file.type.match(/image*/)) {
-            var img = document.createElement("img");
-            img.classList.add("obj");
-            img.file = file;
+        var item = document.getElementById("editBox");
+        item.appendChild(input);
 
-            var item = document.getElementById("widgetDrop_root");
-            item.appendChild(img);
+        var reader = new FileReader();
+        reader.onload = function () {
+            console.log(reader.result);
+            var jsonStr = reader.result;
+            input.value = jsonStr;
 
-            var reader = new FileReader();
-            reader.onload = function () {
-                console.log(reader.result);
-                img.src = reader.result;
+            isJson = function (obj) {
+                var isjson = typeof(obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
+                isjson = !isjson;
+                console.log(isjson);
+                return isjson;
             }
-            reader.readAsDataURL(file);
-        } else {
-            // <input id="File1" type="file" value="" name ="file"/ >
-            var input = document.createElement("input");
-            input.classList.add("obj");
+            if (!isJson(jsonStr)) return;
 
-            var item = document.getElementById("editBox");
-            item.appendChild(input);
-
-            var reader = new FileReader();
-            reader.onload = function () {
-                console.log(reader.result);
-                var jsonStr = reader.result;
-                input.value = jsonStr;
-
-                isJson = function (obj) {
-                    var isjson = typeof(obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
-                    isjson = !isjson;
-                    console.log(isjson);
-                    return isjson;
-                }
-                if (!isJson(jsonStr)) return;
-
-                var jsonObj = eval('(' + jsonStr + ')');
-                console.log(jsonObj);
-                for (var j = 0; j < jsonObj.length; j++) {
-                    alert(jsonObj[j].id);
-                    alert(jsonObj[j].name);
-                }
-                for (var o in jsonObj) {
-                    console.log(o);
-                    console.log(jsonObj[o]);
-                    console.log(typeof(jsonObj[o]));
-                    if (typeof(jsonObj[o]) == "object") {
-                        for (var o1 in jsonObj[o]) {
-                            console.log(o1);
-                            console.log(jsonObj[o][o1]);
-                        }
+            var jsonObj = eval('(' + jsonStr + ')');
+            console.log(jsonObj);
+            for (var j = 0; j < jsonObj.length; j++) {
+                alert(jsonObj[j].id);
+                alert(jsonObj[j].name);
+            }
+            for (var o in jsonObj) {
+                console.log(o);
+                console.log(jsonObj[o]);
+                console.log(typeof(jsonObj[o]));
+                if (typeof(jsonObj[o]) == "object") {
+                    for (var o1 in jsonObj[o]) {
+                        console.log(o1);
+                        console.log(jsonObj[o][o1]);
                     }
                 }
             }
-            reader.readAsText(file);
         }
+        reader.readAsText(file);
     }
 };
--- html5/js/widget/widgetTab.js
+++ html5/js/widget/widgetTab.js
@@ -1,141 +1,134 @@
-var widgetTabController = {
-    name: "widgetTabController",
-    slideSpeed: 200,
+function WidgetTabController() {
+};
+WidgetTabController.name = "WidgetTabController";
+WidgetTabController.slideSpeed = 200;
 
-    init: function (d) {
-        logController.log(this.name, "->", arguments.callee.name);
+WidgetTabController.prototype.init = function (d) {
+    logController.log(this.name, "->", arguments.callee);
 
-        var eventList = new Array();
-        eventList["bindUIAction"] = widgetTabController.bindUIAction;
-        eventList["hideInactive"] = widgetTabController.hideInactive;
-        widgetTabController.tabEvent(eventList);
-    },
+    var eventList = new Array();
+    eventList["bindUIAction"] = this.bindUIAction;
+    eventList["hideInactive"] = this.hideInactive;
+    this.tabEvent(eventList);
+};
+WidgetTabController.prototype.tabEvent = function (eventList) {
+    logController.log(this.name, "->", arguments.callee);
 
-    tabEvent: function (eventList) {
-        logController.log(this.name, "->", arguments.callee.name);
+    var tabList = document.getElementsByClassName("widgetTab");
+    var $tabList = $(tabList);
+    $tabList.each(function (tabIndex) {
+        var $tabItem = $(tabList[tabIndex]);
 
-        var tabList = document.getElementsByClassName("widgetTab");
-        var $tabList = $(tabList);
-        $tabList.each(function (tabIndex) {
-            var $tabItem = $(tabList[tabIndex]);
-
-            var childList = $tabItem.context.childNodes;
-            var $childList = $(childList);
-            var ulTitleGroup = null;
-            var divContentGroup = null;
-            $childList.each(function (childIndex) {
-                var $childItem = $(childList[childIndex]);
-                if ($childItem.is("ul") && $childItem.hasClass("widgetTabTitleGroup")) {
-                    logController.log("ul.widgetTabTitleGroup = %d", tabIndex);
-                    ulTitleGroup = $childItem;
-                } else if ($childItem.is("div") && $childItem.hasClass("widgetTabContentGroup")) {
-                    logController.log("div.widgetTabContentGroup = %d", tabIndex);
-                    divContentGroup = $childItem;
-                }
-            })
-            widgetTabController.titleGroupEvent(ulTitleGroup, divContentGroup, eventList);
-            widgetTabController.contentGroupEvent(ulTitleGroup, divContentGroup, eventList);
-        })
-    },
-
-    titleGroupEvent: function (ulTitleGroup, divContentGroup, eventList) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        var childList = ulTitleGroup.context.childNodes;
+        var childList = $tabItem.context.childNodes;
         var $childList = $(childList);
-        $childList.each(function (childIndex) {
-            var $childItem = $(childList[childIndex]);
-            if ($childItem.hasClass("widgetTabTitle")) {
-                logController.log("widgetTabTitle = %d", $childItem.index());
-
-                var event = eventList["bindUIAction"];
-                event && event($childItem)
-            }
-        })
-    },
-
-    contentGroupEvent: function (ulTitleGroup, divContentGroup, eventList) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        var childList = divContentGroup.context.childNodes;
-        var $childList = $(childList);
-        $childList.each(function (childIndex) {
-            var $childItem = $(childList[childIndex]);
-            if ($childItem.hasClass("widgetTabContent")) {
-                logController.log("widgetTabContent = %d", $childItem.index());
-
-                var event = eventList["hideInactive"];
-                event && event(ulTitleGroup.context.childNodes[childIndex], $childItem);
-            }
-        })
-    },
-
-    bindUIAction: function (titleItem) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        titleItem.on("click", function () {
-            widgetTabController.switchTitle($(this));
-        });
-    },
-
-    hideInactive: function (titleItem, contentItem) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        var $titleItem = $(titleItem);
-        contentItem.hide();
-        if ($titleItem.hasClass("isActive")) {
-            contentItem.show();
-        }
-    },
-
-    switchTitle: function (titleItem) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        var tabItem = titleItem.closest('.widgetTab');
-        if (tabItem.length > 0) {
-            tabItem = tabItem[0];
-        }
-
-        if (!titleItem.hasClass('isActive')) {
-            titleItem.siblings().removeClass('isActive');
-            titleItem.addClass('isActive');
-
-            widgetTabController.showContent(titleItem.index(), $(tabItem));
-        }
-    },
-
-    showContent: function (index, tabItem) {
-        logController.log(this.name, "->", arguments.callee.name);
-
-        var childList = tabItem.context.childNodes;
-        var $childList = $(childList);
+        var ulTitleGroup = null;
         var divContentGroup = null;
         $childList.each(function (childIndex) {
             var $childItem = $(childList[childIndex]);
-            if ($childItem.is("div") && $childItem.hasClass("widgetTabContentGroup")) {
+            if ($childItem.is("ul") && $childItem.hasClass("widgetTabTitleGroup")) {
+                logController.log("ul.widgetTabTitleGroup = %d", tabIndex);
+                ulTitleGroup = $childItem;
+            } else if ($childItem.is("div") && $childItem.hasClass("widgetTabContentGroup")) {
+                logController.log("div.widgetTabContentGroup = %d", tabIndex);
                 divContentGroup = $childItem;
-
-                var contentGroupChildList = divContentGroup.context.childNodes;
-                var $contentGroupChildList = $(contentGroupChildList);
-                $contentGroupChildList.each(function (contentGroupChildIndex) {
-                    var $contentGroupChildItem = $(contentGroupChildList[contentGroupChildIndex]);
-
-                    if ($contentGroupChildItem.hasClass("widgetTabContent")) {
-                        logController.log("widgetTabContent = %d", $contentGroupChildItem.index());
-
-                        if ($contentGroupChildItem.index() == index) {
-                            $contentGroupChildItem.slideDown(widgetTabController.slideSpeed);
-                        } else {
-                            $contentGroupChildItem.slideUp(widgetTabController.slideSpeed);
-                        }
-                        // eventList.each(function(eventIndex) {
-                        //     logController.log("eventList = %d", eventIndex);
-                        //     var eventItem = eventList[eventIndex];
-                        //     eventItem && eventItem($childItem);
-                        // })
-                    }
-                })
             }
         })
+        this.titleGroupEvent(ulTitleGroup, divContentGroup, eventList);
+        this.contentGroupEvent(ulTitleGroup, divContentGroup, eventList);
+    })
+};
+WidgetTabController.prototype.titleGroupEvent = function (ulTitleGroup, divContentGroup, eventList) {
+    logController.log(this.name, "->", arguments.callee);
+
+    var childList = ulTitleGroup.context.childNodes;
+    var $childList = $(childList);
+    $childList.each(function (childIndex) {
+        var $childItem = $(childList[childIndex]);
+        if ($childItem.hasClass("widgetTabTitle")) {
+            logController.log("widgetTabTitle = %d", $childItem.index());
+
+            var event = eventList["bindUIAction"];
+            event && event($childItem)
+        }
+    })
+};
+WidgetTabController.prototype.contentGroupEvent = function (ulTitleGroup, divContentGroup, eventList) {
+    logController.log(this.name, "->", arguments.callee);
+
+    var childList = divContentGroup.context.childNodes;
+    var $childList = $(childList);
+    $childList.each(function (childIndex) {
+        var $childItem = $(childList[childIndex]);
+        if ($childItem.hasClass("widgetTabContent")) {
+            logController.log("widgetTabContent = %d", $childItem.index());
+
+            var event = eventList["hideInactive"];
+            event && event(ulTitleGroup.context.childNodes[childIndex], $childItem);
+        }
+    })
+};
+WidgetTabController.prototype.bindUIAction = function (titleItem) {
+    logController.log(this.name, "->", arguments.callee);
+
+    titleItem.on("click", function () {
+        this.switchTitle($(this));
+    });
+};
+WidgetTabController.prototype.hideInactive = function (titleItem, contentItem) {
+    logController.log(this.name, "->", arguments.callee);
+
+    var $titleItem = $(titleItem);
+    contentItem.hide();
+    if ($titleItem.hasClass("isActive")) {
+        contentItem.show();
     }
+};
+WidgetTabController.prototype.switchTitle = function (titleItem) {
+    logController.log(this.name, "->", arguments.callee);
+
+    var tabItem = titleItem.closest('.widgetTab');
+    if (tabItem.length > 0) {
+        tabItem = tabItem[0];
+    }
+
+    if (!titleItem.hasClass('isActive')) {
+        titleItem.siblings().removeClass('isActive');
+        titleItem.addClass('isActive');
+
+        this.showContent(titleItem.index(), $(tabItem));
+    }
+};
+WidgetTabController.prototype.showContent = function (index, tabItem) {
+    logController.log(this.name, "->", arguments.callee);
+
+    var childList = tabItem.context.childNodes;
+    var $childList = $(childList);
+    var divContentGroup = null;
+    $childList.each(function (childIndex) {
+        var $childItem = $(childList[childIndex]);
+        if ($childItem.is("div") && $childItem.hasClass("widgetTabContentGroup")) {
+            divContentGroup = $childItem;
+
+            var contentGroupChildList = divContentGroup.context.childNodes;
+            var $contentGroupChildList = $(contentGroupChildList);
+            $contentGroupChildList.each(function (contentGroupChildIndex) {
+                var $contentGroupChildItem = $(contentGroupChildList[contentGroupChildIndex]);
+
+                if ($contentGroupChildItem.hasClass("widgetTabContent")) {
+                    logController.log("widgetTabContent = %d", $contentGroupChildItem.index());
+
+                    if ($contentGroupChildItem.index() == index) {
+                        $contentGroupChildItem.slideDown(this.slideSpeed);
+                    } else {
+                        $contentGroupChildItem.slideUp(this.slideSpeed);
+                    }
+                    // eventList.each(function(eventIndex) {
+                    //     logController.log("eventList = %d", eventIndex);
+                    //     var eventItem = eventList[eventIndex];
+                    //     eventItem && eventItem($childItem);
+                    // })
+                }
+            })
+        }
+    })
 };
