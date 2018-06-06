85a6eac2c01772b6b5fa55cdbcefc53385b78770
Teddy <teddy689@qq.com>
2018-06-06 17:42:50 +0800

    1

--- html5/js/business/editor/panel/panelFileBrowser.js
+++ html5/js/business/editor/panel/panelFileBrowser.js
@@ -10,7 +10,33 @@ PanelFileBrowserController.prototype.init = function () {
     $(createBtn).on("click", PanelFileBrowserController.create);
 
     this._widgetSearchController.createSearch(this, createBtn);
+
+    this.initDiv();
 }
 PanelFileBrowserController.create = function () {
     WidgetMenuController.showMenu(this._panelFileBrowserController._menuLeftCreate, null, this._panelFileBrowserController);
 }
+PanelFileBrowserController.prototype.initDiv = function () {
+    var left = document.getElementById("left");
+    var middle = document.getElementById("middle");
+
+    middle.onmousedown = function (e) {
+        var x = (e || event).clientX;
+        middle.left = middle.offsetLeft;
+        document.onmousemove = function (e) {
+            var iT = middle.left + ((e || event).clientX - x);
+            var maxT = document.body.clientWidth;
+            iT < 30 && (iT = 30);
+            iT > maxT && (iT = maxT);
+            middle.style.left = left.style.width = iT + "px";
+            return false
+        };
+        document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null;
+            middle.releaseCapture && middle.releaseCapture()
+        };
+        middle.setCapture && middle.setCapture();
+        return false
+    };
+};
