e5eb79a2366416880c9cfea583caf868ee81ec02
Teddy <teddy689@qq.com>
2018-06-26 14:57:49 +0800

    1

--- html5/js/business/editor/panel/panelMenu.js
+++ html5/js/business/editor/panel/panelMenu.js
@@ -3,7 +3,7 @@ function PanelMenu() {
 
 PanelMenu.prototype.init = function () {
     var panelMenu = document.getElementById("panelMenu");
-    WidgetHtml.addInput(panelMenu, this, "清空历史记录", WidgetHtml._inputType.button, PanelMenu.onClickHistoryClear, null);
+    // WidgetHtml.addInput(panelMenu, this, "清空历史记录", WidgetHtml._inputType.button, PanelMenu.onClickHistoryClear, null);
 }
 PanelMenu.onClickHistoryClear = function (e) {
     WidgetHistory.clear();
