3edd46c40b9b933c44373c671dc7fed71d2450e6
teddy <teddy@teddytekiMacBook-Pro.local>
2018-09-10 10:37:10 +0800

    1

--- nodeJS/js/module/moduleRouter.js
+++ nodeJS/js/module/moduleRouter.js
@@ -25,27 +25,3 @@ ModuleRouter.handle = function(structServer) {
         router.handle(structServer);
     }
 }
-
-// var mysql = require('mysql');
-// var fs = require('fs');
-// // 从数据库拿数据
-// console.log('进来了！');
-// db.query('select * from news', function(err, data) {
-//     if (!err) {
-//         // res.writeHead(200, {'Content-Type':'text/plain'});
-//         // res.end('hello nodeJS 1');
-//         res.write(JSON.stringify(data));
-//         res.end();
-//     }
-// });
-// // 从文件拿数据
-// fs.readFile('www' + req.url, function(err, data) {
-//     console.log('www' + req.url);
-//     if (!err) {
-//         res.write(data);
-//         res.end();
-//     } else {
-//         res.write('404');
-//         res.end();
-//     }
-// });
