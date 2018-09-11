fad75760cda6cb153f8a6e160f3e0d45c307731c
teddy <teddy@teddytekiMacBook-Pro.local>
2018-09-11 16:07:13 +0800

    1

--- nodeJS/js/module/moduleMysql.js
+++ nodeJS/js/module/moduleMysql.js
@@ -35,28 +35,31 @@ ModuleMysql.prototype.handle = function(structServer) {
 // 增、删、改、查
 ModuleMysql.prototype.add = function(structServer) {
     var table = structServer._jsonClient.table;
-    var addSql = 'INSERT INTO ' + table +'(id,name,age) VALUES(0,?,?)';
+    var sql = 'INSERT INTO ' + table + '(id,name,age) VALUES(0,?,?)';
+
+
     var addSql_Param = ['Wilson', 55];
-    structServer._server._mysql.query(addSql, addSql_Param, function(err, result) {
+    structServer._server._mysql.query(sql, addSql_Param, function(err, result) {
         if (err) {
             console.log('[INSERT ERROR] - ', err.message);
             return;
         }       
-        console.log('INSERT ID:', result.insertId);       
-        console.log('INSERT ID:', result);       
+        console.log('INSERT:', result);       
     });
 }
 ModuleMysql.prototype.del = function(structServer) {
-// var userDelSql = 'DELETE FROM node_user WHERE id = 7';
-// db.query(userDelSql, function(err, result) {
-//     if (err) {
-//         console.log('[DELETE ERROR] - ', err.message);
-//         return;
-//     }       
-//     console.log('-------------DELETE--------------');
-//     console.log('DELETE affectedRows', result.affectedRows);
-//     console.log('&&&&&&&&&&&&&&&&&'); 
-// });
+    var sql = 'DELETE FROM ' + table + 'WHERE ' + key + '=' + value;
+
+
+db.query(userDelSql, function(err, result) {
+    if (err) {
+        console.log('[DELETE ERROR] - ', err.message);
+        return;
+    }       
+    console.log('-------------DELETE--------------');
+    console.log('DELETE affectedRows', result.affectedRows);
+    console.log('&&&&&&&&&&&&&&&&&'); 
+});
 }
 ModuleMysql.prototype.up = function(structServer) {
 // var userModSql = 'UPDATE node_user SET name = ?,age = ? WHERE id = ?';
