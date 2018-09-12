0dbd7d201aa6f6e18207437d8bab667039f743ae
teddy <teddy@teddytekiMacBook-Pro.local>
2018-09-12 14:51:24 +0800

    1

--- html5/js/API/APIServer.js
+++ html5/js/API/APIServer.js
@@ -3,16 +3,15 @@
 function APIServer() {
 }
 
-APIServer._module = {};
-APIServer._mysql = {};
-
 // 模块 module
+APIServer._module = {};
 APIServer._module._mysql = "mysql";
 APIServer._module._fileSystem = "fileSystem";
 APIServer._module._formData = "formData";
 APIServer._module._fileBrowser = "fileBrowser";
 
 // 功能 mysql
+APIServer._mysql = {};
 APIServer._mysql._add = "add";
 APIServer._mysql._del = "del";
 APIServer._mysql._up = "up";
