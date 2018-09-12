5b20b0ef1e858ebf1553327ad9e634a9cf63a85a
teddy <teddy@teddytekiMacBook-Pro.local>
2018-09-12 15:00:10 +0800

    1

--- html5/js/widget/base/widgetKey.js
+++ html5/js/widget/base/widgetKey.js
@@ -8,6 +8,28 @@ WidgetKey._panelFileMould = "panelFileMould";
 WidgetKey._panelFileLink = "panelFileLink";
 WidgetKey._panelFileBrowser = "panelFileBrowser";
 
+// APIKey._fileList = "fileList";
+// APIKey._folderList = "folderList";
+WidgetKey._fileName = "fileName";
+WidgetKey._fileContent = "fileContent";
+
+WidgetKey._json = "json";
+// APIKey._jsonShow = "json";
+WidgetKey._jsonMould = "jsonMould";
+// APIKey._jsonMouldShow = "json模版";
+WidgetKey._personalFolder = "personalFolder";
+// APIKey._personalFoldShow = "个人文件夹";
+
+// APIKey._extendJson = ".json";
+// APIKey._extendJsonMd = ".jsonMd";
+
+// APIKey._type = "type";
+WidgetKey._title = "title";
+// APIKey._extend = "extend";
+
+// APIKey._folder = "folder";
+WidgetKey._file = "file";
+
 WidgetKey._key = "key";
 WidgetKey._keyShow = "keyShow";
 
