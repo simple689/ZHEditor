cae472d1c22262dbf5dcf2272a1de847999305d2
teddy <teddy@teddytekiMacBook-Pro.local>
2018-09-12 20:59:14 +0800

    1

--- html5/js/widget/widgetUserLogin.js
+++ html5/js/widget/widgetUserLogin.js
@@ -1,144 +1,35 @@
 function WidgetUserLogin() {
 }
 
-WidgetUserLogin.prototype.create = function (elementParent, pageRoot, page) {
+WidgetUserLogin.prototype.create = function (elementParent) {
     this._divMain = WidgetHtml.addDiv(elementParent);
-    this._divMain.classList.add("widgetMarkedDocMain");
+    this._divMain.classList.add("widgetUserLoginMain");
 
-    this._divLeft = WidgetHtml.addDiv(this._divMain);
-    this._divLeft._widgetMarkedDoc = this;
-    this._divLeft.classList.add("widgetMarkedDocLeft");
+    this._divUserName = WidgetHtml.addDiv(this._divMain);
+    this._divUserName.classList.add("widgetUserLoginUserName");
 
-    this._divMiddle = WidgetHtml.addDiv(this._divMain);
-    this._divMiddle.classList.add("widgetMarkedDocMiddle");
+    var jsonObjCtrl = new JsonObjCtrl(this, null, false, null);
+    // jsonObjCtrl._value = "xxx@xxx.com";
+    WidgetHtml.addInput(this._divUserName, jsonObjCtrl, WidgetHtml._enumInputType._textString);
 
-    this._divRight = WidgetHtml.addDiv(this._divMain);
-    this._divRight.classList.add("widgetMarkedDocRight");
+    this._divUserPWD = WidgetHtml.addDiv(this._divMain);
+    this._divUserPWD.classList.add("widgetUserLoginUserPWD");
 
-    this._currentPageRoot = pageRoot;
-    this._currentPage = page;
+    jsonObjCtrl = new JsonObjCtrl(this, null, false, null);
+    // jsonObjCtrl._value = "********";
+    WidgetHtml.addInput(this._divUserName, jsonObjCtrl, WidgetHtml._enumInputType._password);
 
-    this._currentHash = '';
-    this._renderedPage = '';
+    this._divVerify = WidgetHtml.addDiv(this._divMain);
+    this._divVerify.classList.add("widgetUserLoginVerify");
 
-    this.init(this._divLeft, this._divMiddle, this._divRight);
+    this._divMsg = WidgetHtml.addDiv(this._divMain);
+    this._divMsg.classList.add("widgetUserLoginMsg");
+
+    this.init();
 }
 
-WidgetUserLogin.prototype.init = function (left, middle, right) {
-    middle.onmousedown = function (e) {
-        var x = (e || event).clientX;
-        middle.left = middle.offsetLeft;
-        document.onmousemove = function (e) {
-            var iT = middle.left + ((e || event).clientX - x);
-            var maxT = document.body.clientWidth;
-            iT < 30 && (iT = 30);
-            iT > maxT && (iT = maxT);
-            middle.style.left = left.style.width = iT + "px";
-            return false
-        };
-        document.onmouseup = function () {
-            document.onmousemove = null;
-            document.onmouseup = null;
-            middle.releaseCapture && middle.releaseCapture()
-        };
-        middle.setCapture && middle.setCapture();
-        return false
-    };
-
-    this.initLeft(left);
-    this.initRight(right);
+WidgetUserLogin.prototype.init = function () {
+    jigsaw.init(document.getElementById('captcha'), function () {
+        document.getElementById('msg').innerHTML = '登录成功！'
+    })
 };
-WidgetUserLogin.prototype.initLeft = function (left) {
-    $(left).load("../menu/menuHelp.html", function () {
-        if (!window.Promise) {
-            window.Promise = ES6Promise;
-        }
-        if (!window.fetch) {
-            window.fetch = unfetch;
-        }
-        var widgetMarkedDoc = this._widgetMarkedDoc;
-        widgetMarkedDoc._linkList = this.querySelectorAll('a');
-        widgetMarkedDoc._linkList.forEach(function(link) {
-            link._widgetMarkedDoc = this;
-            link.onclick = function () {
-                var hashStr = decodeURI(this.hash);
-                this._widgetMarkedDoc.hashChange(hashStr);
-            }
-        }, widgetMarkedDoc);
-
-        widgetMarkedDoc.hashChange();
-    });
-};
-WidgetUserLogin.prototype.initRight = function (right) {
-    this._divRightContent = WidgetHtml.addDiv(right);
-    this._divRightContent.classList.add("widgetMarkedDocRightContent");
-    this._divRightContent.classList.add("markdown-body");
-};
-WidgetUserLogin.prototype.hashChange = function(elementHash) {
-    var hash = elementHash;
-    if (elementHash) {
-        hash = elementHash.substr(1);
-    }
-    // var hash = location.hash.slice(1);
-    if (!hash) {
-        hash = 'README.md';
-    }
-    var uri = hash.split('#');
-    if (uri[0].match(/^\//)) {
-        this._currentPage = uri[0].slice(1);
-        if (uri.length > 1) {
-            this._currentHash = uri[1];
-        } else {
-            this._currentHash = '';
-        }
-    } else {
-        this._currentHash = uri[0];
-    }
-    var widgetMarkedDoc = this;
-    this.fetchPage(this._currentPage).then(function () {
-        widgetMarkedDoc.fetchAnchor(widgetMarkedDoc._currentHash)
-    }, widgetMarkedDoc);
-
-    var url = '#/' + this._currentPage + (this._currentHash ? '#' + this._currentHash : '');
-    var fullUrl = window.location.origin + '/' + url;
-
-    this._linkList.forEach(function(link) {
-        link.className = link.href === fullUrl ? 'selected' : '';
-    });
-
-    // history.replaceState('', document.title, url);
-}
-WidgetUserLogin.prototype.fetchPage = function(page) {
-    if (page === this._renderedPage) {
-        return Promise.resolve();
-    }
-    var widgetMarkedDoc = this;
-    var fetchPage = this._currentPageRoot + page;
-    return fetch(fetchPage, widgetMarkedDoc)
-        .then(function (res) {
-            if (!res.ok) {
-                throw new Error('Error ' + res.status + ': ' + res.statusText);
-            }
-            return res.text();
-        })
-        .then(function (text) {
-            widgetMarkedDoc._renderedPage = page;
-            widgetMarkedDoc._divRightContent.innerHTML = marked(text);
-            widgetMarkedDoc._divRightContent.scrollTop = 0;
-        }).catch(function (e) {
-            widgetMarkedDoc._divRightContent.innerHTML = '<p>Oops! There was a problem rendering the page.</p>'
-                + '<p>' + e.message + '</p>';
-        });
-}
-WidgetUserLogin.prototype.fetchAnchor = function(anchor) {
-    if (!anchor) {
-        this._divRightContent.scrollTo(0, 0);
-        return;
-    }
-    var hashElement = document.getElementById(anchor);
-    if (hashElement) {
-        hashElement.scrollIntoView();
-    }
-    var y = this._divRightContent.scrollTop;
-    this._divRightContent.scrollTo(0, y);
-}
