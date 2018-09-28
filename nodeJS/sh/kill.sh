192c687a05e642194e1e74f66ae776eb4b081519
teddy689 <teddy689@gmail.com>
2018-09-28 10:05:20 +0800

    1

--- nodeJS/sh/kill.sh
+++ nodeJS/sh/kill.sh
@@ -1,42 +1,21 @@
-#! /bin/bash
+# ! /bin/bash
 
-search=$1
-#echo $search
-type=$2
-#echo $type
+port=6989
+# echo $port
 
-ps -ef | grep "$search" | grep python
-# ps -ef | grep "$search" | grep -v "$0" | grep -v "grep"
-idListStr=`ps -ef | grep "$search" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
-echo $idListStr
+# lsof -i:"$port"
+idListStr=`lsof -i:"$port" | awk '{print $2}'`
+# echo $idListStr
 
 idList=(${idListStr// / })
 len=${#idList[@]}
-#echo $len
+# echo $len
 
-if [ "$type" = "all" ]
-then
-    echo "type = $type"
-else
-    len=`expr $len - 1`
-fi
-#echo $len
-
-#echo "---------------"
-for((i=0;i<$len;i++));
+echo "---------------"
+for((i=1;i<$len;i++));
 do
 id=${idList[i]}
 kill -9 $id
-#echo "killed $id"
+echo "killed $id"
 done
-#echo "---------------"
-
-:<<!
-for id in $idListStr
-do
-kill -9 $id
-#echo "killed $id"
-done
-!
-
-ps -ef | grep "$search" | grep -v "$0" | grep -v "grep"
+echo "---------------"
